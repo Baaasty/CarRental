71cca2bca9df4b83b32025c36e77a85649e8222a
Baaasty <46427883+Baaasty@users.noreply.github.com>
2022-07-01 08:33:41 +0200

    Bug fixes

--- src/de/wumpe/carrental/CarRental.java
+++ src/de/wumpe/carrental/CarRental.java
@@ -101,56 +101,93 @@ public class CarRental {
 
         try {
             switch (arguments[2].toUpperCase()) {
-                case "CABRIOLET" -> vehicles.put(id, new Cabriolet(arguments[3],
-                        arguments[4],
-                        Long.parseLong(arguments[5]),
-                        Long.parseLong(arguments[6]),
-                        Float.parseFloat(arguments[7]),
-                        arguments[8],
-                        Float.parseFloat(arguments[9]),
-                        Float.parseFloat(arguments[10]),
-                        Boolean.parseBoolean(arguments[11]),
-                        Byte.parseByte(arguments[12]),
-                        Byte.parseByte(arguments[12]),
-                        arguments[13],
-                        Boolean.parseBoolean(arguments[14])));
-                case "VAN" -> vehicles.put(id, new Van(arguments[3],
-                        arguments[4],
-                        Long.parseLong(arguments[5]),
-                        Long.parseLong(arguments[6]),
-                        Float.parseFloat(arguments[7]),
-                        arguments[8],
-                        Float.parseFloat(arguments[9]),
-                        Float.parseFloat(arguments[10]),
-                        Boolean.parseBoolean(arguments[11]),
-                        Byte.parseByte(arguments[12]),
-                        Byte.parseByte(arguments[13]),
-                        Boolean.parseBoolean(arguments[14])));
-                case "PICKUP" -> vehicles.put(id, new Pickup(arguments[3],
-                        arguments[4],
-                        Long.parseLong(arguments[5]),
-                        Long.parseLong(arguments[6]),
-                        Float.parseFloat(arguments[7]),
-                        arguments[8],
-                        Float.parseFloat(arguments[9]),
-                        Float.parseFloat(arguments[10]),
-                        Boolean.parseBoolean(arguments[11]),
-                        Byte.parseByte(arguments[12]),
-                        Byte.parseByte(arguments[13]),
-                        Boolean.parseBoolean(arguments[14])));
-                case "TRUCK" -> vehicles.put(id, new Truck(arguments[3],
-                        arguments[4],
-                        Long.parseLong(arguments[5]),
-                        Long.parseLong(arguments[6]),
-                        Float.parseFloat(arguments[7]),
-                        arguments[8],
-                        Float.parseFloat(arguments[9]),
-                        Float.parseFloat(arguments[10]),
-                        Boolean.parseBoolean(arguments[11]),
-                        Byte.parseByte(arguments[12]),
-                        Double.parseDouble(arguments[13])));
-                default -> System.out.println("Mögliche Types: CABRIOLET, VAN, PICKUP, TRUCK");
+                case "CABRIOLET" -> {
+                    if (arguments.length < 15) {
+                        System.out.println("Überprüfe deine Argumente");
+                        System.out.println("Tipp: 'arguments'");
+                        return;
+                    }
+
+                    vehicles.put(id, new Cabriolet(arguments[3],
+                            arguments[4],
+                            Long.parseLong(arguments[5]),
+                            Long.parseLong(arguments[6]),
+                            Float.parseFloat(arguments[7]),
+                            arguments[8],
+                            Float.parseFloat(arguments[9]),
+                            Float.parseFloat(arguments[10]),
+                            Boolean.parseBoolean(arguments[11]),
+                            Byte.parseByte(arguments[12]),
+                            Byte.parseByte(arguments[12]),
+                            arguments[13],
+                            Boolean.parseBoolean(arguments[14])));
+                }
+                case "VAN" -> {
+                    if (arguments.length < 15) {
+                        System.out.println("Überprüfe deine Argumente");
+                        System.out.println("Tipp: 'arguments'");
+                        return;
+                    }
+
+                    vehicles.put(id, new Van(arguments[3],
+                            arguments[4],
+                            Long.parseLong(arguments[5]),
+                            Long.parseLong(arguments[6]),
+                            Float.parseFloat(arguments[7]),
+                            arguments[8],
+                            Float.parseFloat(arguments[9]),
+                            Float.parseFloat(arguments[10]),
+                            Boolean.parseBoolean(arguments[11]),
+                            Byte.parseByte(arguments[12]),
+                            Byte.parseByte(arguments[13]),
+                            Boolean.parseBoolean(arguments[14])));
+                }
+                case "PICKUP" -> {
+                    if (arguments.length < 15) {
+                        System.out.println("Überprüfe deine Argumente");
+                        System.out.println("Tipp: 'arguments'");
+                        return;
+                    }
+
+                    vehicles.put(id, new Pickup(arguments[3],
+                            arguments[4],
+                            Long.parseLong(arguments[5]),
+                            Long.parseLong(arguments[6]),
+                            Float.parseFloat(arguments[7]),
+                            arguments[8],
+                            Float.parseFloat(arguments[9]),
+                            Float.parseFloat(arguments[10]),
+                            Boolean.parseBoolean(arguments[11]),
+                            Byte.parseByte(arguments[12]),
+                            Byte.parseByte(arguments[13]),
+                            Boolean.parseBoolean(arguments[14])));
+                }
+                case "TRUCK" -> {
+                    if (arguments.length < 14) {
+                        System.out.println("Überprüfe deine Argumente");
+                        System.out.println("Tipp: 'arguments'");
+                        return;
+                    }
+
+                    vehicles.put(id, new Truck(arguments[3],
+                            arguments[4],
+                            Long.parseLong(arguments[5]),
+                            Long.parseLong(arguments[6]),
+                            Float.parseFloat(arguments[7]),
+                            arguments[8],
+                            Float.parseFloat(arguments[9]),
+                            Float.parseFloat(arguments[10]),
+                            Boolean.parseBoolean(arguments[11]),
+                            Byte.parseByte(arguments[12]),
+                            Double.parseDouble(arguments[13])));
+                }
+                default -> {
+                    System.out.println("Mögliche Types: CABRIOLET, VAN, PICKUP, TRUCK");
+                    return;
+                }
             }
+
+            System.out.printf("Das Fahrzeug '%d' wurde entfernt\n", id);
         } catch (NumberFormatException exception) {
             System.out.println("Überprüfe deine Argumente");
             System.out.println("Tipp: 'arguments'");
